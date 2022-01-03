679091071a6c696809d330a3b3def5dc13e0cd2d
Vinayak Mali <malivinayak012@gmail.com>
2022-01-03 17:27:28 +0530

    Completion of HTML and JS

--- cal.js
+++ cal.js
@@ -1,5 +1,20 @@
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
-var result = document.getElementById('result');
+var resultField = document.getElementById('resultField');
 
 var form = document.getElementById('xIsWhatPercentOfY');
+
+form.addEventListener('submit', function(event) {
+    if (!num1.value || !num2.value) {
+        alert("Please enter values in field");
+    } else {
+        var x = parseFloat(num1.value);
+        var y = parseFloat(num2.value);
+
+        var result = x / y;
+        var percent = result * 100;
+
+        resultField.innerText = "Result: " + percent + " %";
+        event.preventDefault();
+    }
+});
