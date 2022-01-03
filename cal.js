e1893bcbcfe6c5a95ca4eec71e552db58a42ee4e
Vinayak Mali <malivinayak012@gmail.com>
2022-01-03 17:12:51 +0530

    Grabbing form with ID

--- cal.js
+++ cal.js
@@ -1,3 +1,5 @@
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
 var result = document.getElementById('result');
+
+var form = document.getElementById('xIsWhatPercentOfY');
